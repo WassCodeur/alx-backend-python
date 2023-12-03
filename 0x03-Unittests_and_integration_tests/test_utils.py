81c5091629ba34d3f5460e3083d48415e0c3051b
WassCodeur <wachioubouraima56@gmail.com>
2023-12-03 17:26:11 +0000

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,9 @@
 """unit test"""
 
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -23,6 +24,32 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path, expected):
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    @patch('utils.requests.get')
+    def test_get_json(self, mock_get):
+        test_cases = [
+                {
+                    "test_url": "http://example.com",
+                    "test_payload": {"payload": True}
+                    },
+                {
+                    "test_url": "http://holberton.io",
+                    "test_payload": {"payload": False}
+                    }
+                ]
+        for test_case in test_cases:
+            with self.subTest(test_case=test_case):
+                mock_get.return_value = Mock()
+                mock_get.return_value.json.return_value =\
+                    test_case['test_payload']
+
+                result = get_json(test_case['test_url'])
+
+                mock_get.assert_called_once_with(test_case['test_url'])
+
+                self.assertEqual(result, test_case['test_payload'])
 
 
 if __name__ == "__main__":
