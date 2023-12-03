d2d5bd8a6e05a2b3918e3903b898b7fd6879d06c
WassCodeur <wachioubouraima56@gmail.com>
2023-12-03 22:40:29 +0000

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ from parameterized import parameterized
 from unittest.mock import patch
 import requests
 import unittest
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -52,6 +52,35 @@ class TestGetJson(unittest.TestCase):
 
         mock.assert_called_once()
 
+        patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        patcher = patch.object(TestClass, "a_method")
+        mock_test = patcher.start()
+
+        newTestClass = TestClass()
+        first_call_result = newTestClass.a_property()
+
+        mock_test.assert_called_once()
+        mock_test.reset_mock()
+
+        newTestClass = TestClass()
+        second_call_result = newTestClass.a_property()
+
+        mock_test.assert_not_called()
+
+        self.assertEqual(first_call_result, second_call_result)
         patcher.stop()
 
 
